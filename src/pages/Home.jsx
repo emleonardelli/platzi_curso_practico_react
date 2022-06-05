f4c05b283ea469989b485b3a2b9b628790d54934
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2022-06-05 11:17:56 -0300

    feat(template): Atomic design y templates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import ProductList from '../containers/ProductList';
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  );
+	return (
+		<>
+			<ProductList />
+		</>
+	);
 }
 
 export default Home;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import Login from '../pages/Login';
 import Home from '../pages/Home';
+import SendEmail from '../pages/SendEmail';
+import PasswordRecovery from '../pages/PasswordRecovery';
 import NotFound from '../pages/NotFound';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import '../styles/global.css';
 
 const App = () => {
@@ -12,10 +18,16 @@ const App = () => {
 		<BrowserRouter>
 			<Layout>
 				<Routes>
-					<Route path="/" element={<Home />}/>
-					<Route path="/login" element={<Login />}/>
-					<Route path="/recovery-password" element={<RecoveryPassword />}/>
-					<Route element={<NotFound />}/>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/password-recovery" element={<PasswordRecovery />} />
+					<Route path="/send-email" element={<SendEmail />} />
+					<Route path="/new-password" element={<NewPassword />} />
+					<Route path="/account" element={<MyAccount />} />
+					<Route path="/signup" element={<CreateAccount />} />
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/orders" element={<Orders />} />
+					<Route element={<NotFound />} />
 				</Routes>
 			</Layout>
 		</BrowserRouter>
